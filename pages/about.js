80dbbe43f40bfe062a8f4579cfc662e0f1717a84
Your Name Main <you@example.com>
2022-12-24 23:09:05 -0800

    about us added

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+const about = () => {
+    return (
+        <div>
+            <h1 className="text-3xl text-center">About Page</h1>  
+        </div>
+    );
+};
+
+export default about;
---
+++ pages/booking.js
@@ -0,0 +1,10 @@
+
+const booking = () => {
+    return (
+        <div>
+            <h2 className="text-2xl text-center">Booking Component</h2>
+        </div>
+    );
+};
+
+export default booking;
